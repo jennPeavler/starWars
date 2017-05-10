f294856047208dd8af4cab660d62e8487592e71b
Dave Hubertus <davehubertus@gmail.com>
2017-05-10 17:12:04 -0600

    dh-app-testing

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -8,7 +8,7 @@ import peopleData from './mockdata/peopleData.js'
 import planetData from './mockdata/planetData.js'
 import vehicleData from './mockdata/vehicleData.js'
 
-describe('App', () => {
+describe('App Component API Calls', () => {
   const waitingFunc = () => {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
@@ -17,10 +17,10 @@ describe('App', () => {
     })
   }
 
-  // afterEach(() => {
-  //   expect(fetchMock.calls().unmatched).toEqual([])
-  //   fetchMock.restore()
-  // })
+  afterEach(() => {
+    fetchMock.restore()
+    expect(fetchMock.calls().unmatched).toEqual([])
+  })
 
   it('renders without crashing', () => {
     const div = document.createElement('div')
@@ -31,9 +31,8 @@ describe('App', () => {
     fetchMock.get('http://swapi.co/api/films' , {
         status: 500,
       })
-      .catch(
-        console.log('noooooo')
-      )
+      .catch()
+
 
     const wrapper = mount(<App />)
 
@@ -47,24 +46,21 @@ describe('App', () => {
         status: 200,
         body: filmData
       })
-      .catch(
-        console.log('noooooo')
-      )
+      .catch()
 
       const wrapper = mount(<App />)
 
       await waitingFunc()
-      console.log(wrapper.state())
+
       expect(wrapper.state('quotes').length).toEqual(1)
   })
 
-  it('should return error prop in people state if it does not fetch the data from the people api', async () => {
+  it('should return error message in people state if it does not fetch the data from the people api', async () => {
     fetchMock.get('http://swapi.co/api/people' , {
         status: 500,
       })
-      .catch(
-        console.log('noooooo')
-      )
+      .catch()
+
 
     const wrapper = mount(<App />)
 
@@ -73,14 +69,13 @@ describe('App', () => {
     expect(wrapper.state('people')).toEqual('Need people, you do')
   })
 
-  it.only('should have people in state if people API call returns 200', async () => {
+  it('should have people in state if people API call returns 200', async () => {
     fetchMock.get('http://swapi.co/api/people' , {
         status: 200,
         body: peopleData
       })
-      .catch(
-        console.log('noooooo')
-      )
+      .catch()
+
 
       const wrapper = mount(<App />)
 
@@ -89,4 +84,165 @@ describe('App', () => {
       expect(keys.length).toEqual(10)
   })
 
+  it('should return error message in planets state if it does not fetch the data from the planet api', async () => {
+    fetchMock.get('http://swapi.co/api/planets/' , {
+        status: 500,
+      }).catch()
+
+    const wrapper = mount(<App />)
+
+    await waitingFunc()
+
+    expect(wrapper.state('planets')).toEqual('Need planets, you do')
+  })
+
+  it('should have planets in state if planets API call returns 200', async () => {
+    fetchMock.get('http://swapi.co/api/planets/' , {
+        status: 200,
+        body: planetData
+      }).catch()
+
+
+      const wrapper = mount(<App />)
+
+      await waitingFunc()
+
+      let keys = Object.keys(wrapper.state('planets'))
+      expect(keys.length).toEqual(10)
+  })
+
+  it('should return error message in vehicles state if it does not fetch the data from the vehicles api', async () => {
+    fetchMock.get('http://swapi.co/api/vehicles/' , {
+        status: 500,
+      }).catch()
+
+    const wrapper = mount(<App />)
+
+    await waitingFunc()
+
+    expect(wrapper.state('vehicles')).toEqual('Invisbile vehicles, you have!')
+  })
+
+  it('should have planets in state if planets API call returns 200', async () => {
+    fetchMock.get('http://swapi.co/api/vehicles/' , {
+        status: 200,
+        body: vehicleData
+      }).catch()
+
+
+      const wrapper = mount(<App />)
+
+      await waitingFunc()
+
+      let keys = Object.keys(wrapper.state('vehicles'))
+      expect(keys.length).toEqual(10)
+  })
+})
+
+describe('App handleClick function', () => {
+  beforeEach = () => {
+    // const wrapper = mount(<App />)
+  }
+
+  it('should change lastClicked state to people when people button is clicked', () => {
+    const wrapper = mount(<App />)
+
+    const peopleButton = wrapper.find('.people-btn')
+
+    peopleButton.simulate('click')
+
+    expect(wrapper.state('lastClick')).toEqual('people')
+  })
+
+  it('should change lastClicked state to planets when planets button is clicked', () => {
+    const wrapper = mount(<App />)
+
+    const planetsButton = wrapper.find('.planets-btn')
+
+    planetsButton.simulate('click')
+
+    expect(wrapper.state('lastClick')).toEqual('planets')
+  })
+
+  it('should change lastClicked state to vehicles when vehicles button is clicked', () => {
+    const wrapper = mount(<App />)
+
+    const vehiclesButton = wrapper.find('.vehicles-btn')
+
+    vehiclesButton.simulate('click')
+
+    expect(wrapper.state('lastClick')).toEqual('vehicles')
+  })
+
+  it('should change lastClicked state to favorites when favorites button is clicked', () => {
+    const wrapper = mount(<App />)
+
+    const favoritesButton = wrapper.find('#favorites-btn')
+
+    favoritesButton.simulate('click')
+
+    expect(wrapper.state('lastClick')).toEqual('favorites')
+  })
+
+})
+
+describe('App handleToggle function', () => {
+  const waitingFunc = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve()
+      },1000)
+    })
+  }
+
+  beforeEach = () => {
+    fetchMock.get('http://swapi.co/api/films/' , {
+        status: 200,
+        body: filmData
+      })
+      .catch()
+
+      fetchMock.get('http://swapi.co/api/planets/' , {
+          status: 200,
+          body: planetData
+        })
+        .catch()
+
+      fetchMock.get('http://swapi.co/api/vehicles/' , {
+          status: 200,
+          body: vehicleData
+        })
+        .catch()
+
+      fetchMock.get('http://swapi.co/api/people/' , {
+          status: 200,
+          body: peopleData
+        })
+        .catch()
+
+
+  }
+
+  afterEach = () => {
+    fetchMock.restore()
+    expect(fetchMock.calls().unmatched).toEqual([])
+  }
+
+  it.only('should add the correct card obj to favorites when the card is clicked', async () => {
+    const wrapper = mount(<App />)
+
+    await waitingFunc()
+
+    const peopleButton = wrapper.find('.people-btn')
+    peopleButton.simulate('click')
+
+    const CardDisplay = wrapper.find('CardDisplay')
+    const cardToBeClicked = wrapper.find('#C-3PO')
+
+    cardToBeClicked.simulate('click')
+
+    expect(cardToBeClicked.props().id).toBe('C-3PO')
+    expect(wrapper.state('favorites')[0].name).toEqual('C-3PO')
+    expect(wrapper.state('favorites')[0].name).not.toEqual('Luke Skywalker')
+  })
 })
--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -3,9 +3,9 @@ import React from 'react'
 export const Controls = ( {getPeople, getVehicles, getPlanets, handleClick }) => {
   return (
     <div>
-      <button className='button' onClick= {() => handleClick('people')}>PEOPLE</button>
-      <button className='button' onClick= {() => handleClick('planets')}>PLANETS</button>
-      <button className='button' onClick= {() => handleClick('vehicles')}>VEHICLES</button>
+      <button className='button people-btn' onClick= {() => handleClick('people')}>PEOPLE</button>
+      <button className='button planets-btn' onClick= {() => handleClick('planets')}>PLANETS</button>
+      <button className='button vehicles-btn' onClick= {() => handleClick('vehicles')}>VEHICLES</button>
 
 
     </div>
